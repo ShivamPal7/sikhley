235105bdd6cdfdf48f2e1c1df17071803f854e54
anikett35 <aniketbedwal18@gmail.com>
2024-10-10 21:55:42 +0530

    Aniket Test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-semibold text-sky-700">Hello World</p>
+      <p className="text-3xl font-semibold text-yellow-700">Hello World</p>
+      <p className="text-3xl font-semibold text-red-700">aniket test</p>
       <Button>Click me</Button>
     </div>
     
