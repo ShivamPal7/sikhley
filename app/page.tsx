551a375033db0d0b86ed2d974bffbd0b548ea0af
Vivek <er.khedkarvivek@gmail.com>
2024-10-10 22:40:39 +0530

    Vivek

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-semibold text-sky-700">Hello World</p>
+     
+      <p className="text-3xl font-semibold text-sky-700">CodingSlide Here!</p>
       <Button>Click me</Button>
     </div>
     
